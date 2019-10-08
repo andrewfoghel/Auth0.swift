0410b46270782a42098714ea00495a0a5f53dbcc
andrewfoghel <andrewfoghel@gmail.com>
2019-10-08 18:37:10 -0400

    slim down SilentSafariViewController in favor of FullScreenSilentSafariViewController

--- Auth0/SilentSafariViewController.swift
+++ Auth0/SilentSafariViewController.swift
@@ -24,26 +24,78 @@ import UIKit
 import SafariServices
 
 class SilentSafariViewController: SFSafariViewController, SFSafariViewControllerDelegate {
-    var onResult: (Bool) -> Void = { _ in }
-
-    required init(url URL: URL, callback: @escaping (Bool) -> Void) {
+    
+    required init(url URL: URL) {
         #if swift(>=3.2)
-            if #available(iOS 11.0, *) {
-                super.init(url: URL, configuration: SFSafariViewController.Configuration())
-            } else {
-                super.init(url: URL, entersReaderIfAvailable: false)
-            }
-        #else
+        if #available(iOS 11.0, *) {
+            super.init(url: URL, configuration: SFSafariViewController.Configuration())
+        } else {
             super.init(url: URL, entersReaderIfAvailable: false)
+        }
+        #else
+        super.init(url: URL, entersReaderIfAvailable: false)
         #endif
-
-        self.onResult = callback
-        self.delegate = self
+        
         self.view.alpha = 0.05 // Apple does not allow invisible SafariViews, this is the threshold.
         self.modalPresentationStyle = .overCurrentContext
     }
+}
 
+class FullScreenSilentSafariViewController: UIViewController, SFSafariViewControllerDelegate {
+    
+    // MARK: Dependencies
+    
+    private var onResult: (Bool) -> Void = { _ in }
+    private var safariVC: SilentSafariViewController
+
+    // MARK: UI
+    
+    override var prefersStatusBarHidden: Bool {
+        return true
+    }
+    
+    // MARK: Init
+    
+    required init(url URL: URL, callback: @escaping (Bool) -> Void) {
+        self.safariVC = SilentSafariViewController(url: URL)
+        self.onResult = callback
+        super.init(nibName: nil, bundle: nil)
+        
+        view.backgroundColor = .clear
+        modalPresentationStyle = .overCurrentContext
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    // MARK: View
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        
+        safariVC.delegate = self
+        
+        // Kind of a hack, in that we really aren't removing the navbar
+        // Rather we are adjusting the starting point of the vpc object
+        // so it appears as the navbar is hidden
+        present(safariVC, animated: false) {
+            var frame = self.safariVC.view.frame
+            let OffsetY: CGFloat = 64
+            
+            frame.origin = CGPoint(x: frame.origin.x, y: frame.origin.y - OffsetY)
+            frame.size = CGSize(width: frame.size.width, height: frame.size.height + OffsetY)
+            self.safariVC.view.frame = frame
+        }
+    }
+    
+    // MARK: SFSafariViewControllerDelegate
+    
     func safariViewController(_ controller: SFSafariViewController, didCompleteInitialLoad didLoadSuccessfully: Bool) {
-        controller.dismiss(animated: false) { self.onResult(didLoadSuccessfully) }
+        controller.dismiss(animated: false) { [weak self] in
+            self?.dismiss(animated: true) { [weak self] in
+                self?.onResult(didLoadSuccessfully)
+            }
+        }
     }
 }
